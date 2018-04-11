23b93923b19e51738298e76aadbf6c6d02adf8f4
simon0420 <simon.bec@live.de>
2018-04-11 13:06:49 +0200

    code reuse increased

--- src/components/pages/upload/dynamic.js
+++ src/components/pages/upload/dynamic.js
@@ -8,6 +8,7 @@ import{
     Route,
     Link
 } from 'react-router-dom';
+import BMenu from "../../menuComponent/menu";
 
 var styles = {
     bmBurgerButton: {
@@ -48,31 +49,11 @@ class Dynamic extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            data: makeData(),
-            menuOpen: false,
         }
     }
 
     showSettings (event){
         event.preventDefault();
-    }
-
-    // This keeps your state in sync with the opening/closing of the menu
-    // via the default means, e.g. clicking the X, pressing the ESC key etc.
-    handleStateChange (state) {
-        this.setState({menuOpen: state.isOpen})
-    }
-
-    // This can be used to close the menu, e.g. when a user clicks a menu item
-    closeMenu () {
-        this.setState({menuOpen: false})
-    }
-
-    // This can be used to toggle the menu, e.g. when using a custom icon
-    // Tip: You probably want to hide either/both default icons if using a custom icon
-    // See https://github.com/negomi/react-burger-menu#custom-icons
-    toggleMenu () {
-        this.setState({menuOpen: !this.state.menuOpen})
     }
 
     render() {
@@ -86,15 +67,7 @@ class Dynamic extends Component {
             <div id="toolbarcontainter">
                 <Toolbar />
                 <div id="outer-container">
-                    <Menu styles={ styles } noOverlay className="menucontent" pageWrapId="page-wrap" >
-                        <p>Navigation</p>
-                        <a id="home" className="menu-item" href="/overview"><i class="ion-home" aria-hidden="true"></i> Home</a>
-                        <a id="market" className="menu-item" href="/market"><i class="ion-bag" aria-hidden="true"></i> Market</a>
-                        <a id="data" className="menu-item" href="/data"><i class="ion-stats-bars" aria-hidden="true"></i> My Data</a>
-                        <a id="integration" className="menu-item" href="/integration"><i class="ion-shuffle" aria-hidden="true"></i> Integration</a>
-                        <a id="upload" className="menu-item" href="/upload"><i class="ion-upload" aria-hidden="true"></i> Upload</a>
-                        <a id="services" className="menu-item--small" href="/services"><i class="ion-star" aria-hidden="true"></i> Services</a>
-                    </Menu>
+                    <BMenu wrap={"page-wrap"} />
                     <main id="page-wrap">
                         <div className="datacontainer">
                             <div className="banner">
